ce94f6e29e3c496ebdd166e368a343bb4a8cfbe0
junji <junji@maggy.(none)>
2013-01-18 19:34:30 +0900

    Modified mouse-pattern

--- script/mousemove.cpp
+++ script/mousemove.cpp
@@ -16,8 +16,8 @@ main(int argc,char** argv){
   }
   
   vnc.init(argv[1],atoi(argv[2]),argv[3]);
-  for(int i=0;i<1024;i+=128){
-    vnc.set_point(i,i,0);
+  for(int i=0;i<1024*100;i++){
+    vnc.set_point(i%1024,i/768,0);
   }
   vnc.close();
   return 0;
